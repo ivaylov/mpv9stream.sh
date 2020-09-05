4865c40eed34b3770305263f1b243a1501f4bc56
איוואילו <3480283+ivaylov@users.noreply.github.com>
2020-09-05 17:12:34 +0300

    Add files via upload

---
+++ mpv9tv.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+mpv -no-border --volume=0 -really-quiet --ontop --geometry=640x360+0%+0% --ytdl-format="[height<360]" https://www.youtube.com/watch?v=21X5lGlDOfg&
+
+mpv -no-border --volume=0 -really-quiet --ontop --geometry=640x360+50%+0% --ytdl-format="[height<360]" https://www.youtube.com/watch?v=coYw-eVU0Ks&
+
+mpv -no-border --volume=0 -really-quiet --ontop --geometry=640x360+100%+0% --ytdl-format="[height<360]" https://www.youtube.com/watch?v=dp8PhLsUcFE&
+
+
+
+mpv -no-border --volume=0 -really-quiet --ontop --geometry=640x360+0%+50% --ytdl-format="[height<360]" https://www.youtube.com/watch?v=NvCSr7qzAAM&
+
+mpv -no-border --volume=0 -really-quiet --ontop --geometry=640x360+50%+50% --ytdl-format="[height<360]" https://www.youtube.com/watch?v=NvqKZHpKs-g&
+
+mpv -no-border --volume=0 -really-quiet --ontop --geometry=640x360+100%+50% --ytdl-format="[height<360]" https://www.youtube.com/watch?v=w_Ma8oQLmSM&
+
+
+
+mpv -no-border --volume=0 -really-quiet --ontop --geometry=640x360+0%+100% --ytdl-format="[height<360]" https://www.youtube.com/watch?v=8T9SFZDP60Q&
+
+mpv -no-border --volume=0 -really-quiet --ontop --geometry=640x360+50%+100% --ytdl-format="[height<360]" https://www.youtube.com/watch?v=9Auq9mYxFEE&
+
+mpv -no-border --volume=0 -really-quiet --ontop --geometry=640x360+100%+100% --ytdl-format="[height<360]" https://www.youtube.com/watch?v=XEJM4Hcgd3M&
