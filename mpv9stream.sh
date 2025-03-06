a24b1f02c15241a98c741b2c7cde64dd3c3100f2
איוואילו <3480283+ivaylov@users.noreply.github.com>
2025-03-07 00:36:22 +0200

    Update mpv9stream.sh

--- mpv9stream.sh
+++ mpv9stream.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-mpv -no-border --volume=0 -really-quiet --ontop --geometry=640x360+0%+0% --ytdl-format="[height<360]" https://www.youtube.com/watch?v=21X5lGlDOfg&
+mpv -no-border --volume=0 -really-quiet --ontop --geometry=640x360+0%+0% --ytdl-format="[height<360]" https://www.youtube.com/live/sFLv5z9pCOc?si=0RA9ETcs9NP5eh5C&
 
-mpv -no-border --volume=0 -really-quiet --ontop --geometry=640x360+50%+0% --ytdl-format="[height<360]" https://www.youtube.com/watch?v=coYw-eVU0Ks&
+mpv -no-border --volume=0 -really-quiet --ontop --geometry=640x360+50%+0% --ytdl-format="[height<360]" https://www.youtube.com/live/pykpO5kQJ98?si=g0eTPiKI8p1Ox6Ws&
 
-mpv -no-border --volume=0 -really-quiet --ontop --geometry=640x360+100%+0% --ytdl-format="[height<360]" https://www.youtube.com/watch?v=dp8PhLsUcFE&
-
-
-
-mpv -no-border --volume=0 -really-quiet --ontop --geometry=640x360+0%+50% --ytdl-format="[height<360]" https://www.youtube.com/watch?v=W1ilCy6XrmI&
-
-mpv -no-border --volume=0 -really-quiet --ontop --geometry=640x360+50%+50% --ytdl-format="[height<360]" https://www.youtube.com/watch?v=HeTWwH1a0CQ&
-
-mpv -no-border --volume=0 -really-quiet --ontop --geometry=640x360+100%+50% --ytdl-format="[height<360]" https://www.youtube.com/watch?v=w_Ma8oQLmSM&
+mpv -no-border --volume=0 -really-quiet --ontop --geometry=640x360+100%+0% --ytdl-format="[height<360]" https://www.youtube.com/live/_hZTq8BbHtI?si=ngoljgjQq-8Scu1n&
 
 
 
-mpv -no-border --volume=0 -really-quiet --ontop --geometry=640x360+0%+100% --ytdl-format="[height<360]" https://www.youtube.com/watch?v=8T9SFZDP60Q&
+mpv -no-border --volume=0 -really-quiet --ontop --geometry=640x360+0%+50% --ytdl-format="[height<360]" https://www.youtube.com/live/iEpJwprxDdk?si=AnOuKMKRPUcXumXo&
 
-mpv -no-border --volume=0 -really-quiet --ontop --geometry=640x360+50%+100% --ytdl-format="[height<360]" https://www.youtube.com/watch?v=9Auq9mYxFEE&
+mpv -no-border --volume=0 -really-quiet --ontop --geometry=640x360+50%+50% --ytdl-format="[height<360]" https://www.youtube.com/live/wfAa1GiNdgM?si=lI7XyXEGAp-IF3x2&
 
-mpv -no-border --volume=0 -really-quiet --ontop --geometry=640x360+100%+100% --ytdl-format="[height<360]" https://www.youtube.com/watch?v=XEJM4Hcgd3M&
+mpv -no-border --volume=0 -really-quiet --ontop --geometry=640x360+100%+50% --ytdl-format="[height<360]" https://www.youtube.com/live/RDW1fTeutDo?si=feMWT3cmllCC-gF3&
+
+
+
+mpv -no-border --volume=0 -really-quiet --ontop --geometry=640x360+0%+100% --ytdl-format="[height<360]" https://www.youtube.com/live/pqBZkqwL3lA?si=3MFJXiY6QJypLUXE&
+
+mpv -no-border --volume=0 -really-quiet --ontop --geometry=640x360+50%+100% --ytdl-format="[height<360]" https://www.youtube.com/live/CG2XDTgvZPo?si=hVChLTzjEye60xMm&
+
+mpv -no-border --volume=0 -really-quiet --ontop --geometry=640x360+100%+100% --ytdl-format="[height<360]" https://www.youtube.com/live/Ap-UM1O9RBU?si=ZGfZFBzoT2GGgsZI&
